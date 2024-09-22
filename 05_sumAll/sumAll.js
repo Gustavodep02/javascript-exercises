0fb2ca444afb50d1a4c9abd88c30efda08aa81b9
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-09-22 14:08:02 -0300

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1,num2) {
+    if (num1<0 || num2<0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
+    let result=0;
+if (num1>num2){
+    for(let i =num2;i<=num1;i++){ 
+        result = result+i;
+    }
+    }else {
+        for(let i =num1;i<=num2;i++){
+            result = result+i;
+        }
+    }
+        return result;
+    
+}
 
 // Do not edit below this line
 module.exports = sumAll;
