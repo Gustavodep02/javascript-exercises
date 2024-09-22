12806c48da02f13594f472e28924aa8b84495e1c
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-09-22 11:43:42 -0300

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    let size = string.length;
+    let result = "";
+    for(let i = size-1; i>=0; i--){
+        result = result + string[i];
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = reverseString;
