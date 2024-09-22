040dead27bb5dede29b98acf143249cdfea64adf
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-09-22 14:38:47 -0300

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFar) {
+  let celsius = (5/9)* (temperatureFar - 32);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCel) {
+  let fahrenheit = (temperatureCel *1.8) + 32;
+  return parseFloat(fahrenheit.toFixed(1)); 
 };
 
 // Do not edit below this line
