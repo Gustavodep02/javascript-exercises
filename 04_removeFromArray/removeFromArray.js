a144bd0c149d8ec92b8195294768cfe60ae8b4be
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-09-22 11:58:21 -0300

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,remove,remove2,remove3,remove4) {
+    let result = [];
+    let size = array.length;
+    for(let i =0;i<size;i++){
+        if(array[i] === remove || array[i] === remove2 || array[i] === remove3 || array[i] === remove4){
+            continue;
+        }else{
+        result.push(array[i]);
+    }
+}
+    return result;
 };
 
 // Do not edit below this line
