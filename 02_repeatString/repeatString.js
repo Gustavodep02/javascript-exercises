b2c40868e457717af13db967ff981959c3c0bfa7
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-09-22 11:39:26 -0300

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,times) {
+    if(times <0){
+        return "ERROR";
+    }
+    let i =0;
+    let result ="";
+    while(i<times){
+        i++;
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
