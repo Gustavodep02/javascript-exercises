d92919a18fe0d81fe22e573b3de6057947a5941d
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-10-08 15:31:17 -0300

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0) {return 0}else if (num <0){return "OOPS"};
+    let fib = [];
+    let a = 1;
+    let b = 1;
+    for (let i=0;i<26;i++){
+        fib.push(a);
+        let temp = a;
+        a = b;
+        b = temp + b;
+    }
+    return fib[num-1];
 };
 
 // Do not edit below this line
