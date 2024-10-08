21c90afcd3da00c36b9a879bd927ae912b6dd0e1
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-10-08 15:39:19 -0300

    Solve Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let length = arr.length;
+    let oldestAge =0;
+    let oldestPerson = arr[0];
+    for (let i =0; i<length;i++){
+        if (!arr[i].yearOfDeath){
+            arr[i].yearOfDeath = new Date().getFullYear();
+        }
+        let age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        if (age>oldestAge){
+            oldestAge = age;
+            oldestPerson = arr[i];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
